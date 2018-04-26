419eb636ea42071e2d555c16b7cd0976376fd718
Sasha Romanov <22565619+aluminiumi@users.noreply.github.com>
2018-04-25 21:19:07 -0500

    added mbc data as sql and script to generate

---
+++ convert_csv_to_sql.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#Vasilis Vloutis
+
+#import math
+import os.path
+import sys
+
+def main(argv):
+    args = argv[1:]
+   
+    csvfile = open("movie_data.csv", "r")
+    linenum = 0
+
+    program = csvfile.read().split("\n")
+    print("USE mbc;");
+
+    while linenum < len(program)-1:
+        line = program[linenum].strip()
+        #print("Entry on line "+str(linenum)+": "+line)
+        inst = line.split(",")
+        if inst[0] == "":
+            #blank line read, do nothing
+            #print("blank line read")
+            break
+        else:
+            #print(inst[2])
+            print("INSERT INTO mbc (id,release_date,revenue,title,vote_average) VALUES ("+inst[0]+", \'"+inst[1]+"\', "+inst[2]+", \'"+inst[3]+"\', "+inst[4]+");")
+
+        linenum+=1
+
+    csvfile.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
