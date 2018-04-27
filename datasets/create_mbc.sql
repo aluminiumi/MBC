c74260535b70c13e6533612a66114595ca2899a7
Sasha Romanov <22565619+aluminiumi@users.noreply.github.com>
2018-04-27 15:23:35 -0500

    added foreign key to sql file

--- datasets/create_mbc.sql
+++ datasets/create_mbc.sql
@@ -2,17 +2,19 @@
 
 USE mbc;
 
+CREATE TABLE Months
+(
+   month int NOT NULL PRIMARY KEY,
+   quarter int NOT NULL
+);
+
 CREATE TABLE Movies
 (
    id int NOT NULL PRIMARY KEY,
    release_date varchar(3) NOT NULL,
    revenue bigint NOT NULL,
    title varchar(120),
-   vote_average float NOT NULL
+   vote_average float NOT NULL,
+   FOREIGN KEY (release_date) REFERENCES Months(month)
 );
 
-CREATE TABLE Months
-(
-   month int NOT NULL PRIMARY KEY,
-   quarter int NOT NULL
-);
