aac0684fa385b4cffccd8304c2d5b2230821e791
Sasha Romanov <22565619+aluminiumi@users.noreply.github.com>
2018-04-27 15:59:47 -0500

    added views to ddl

--- datasets/create_mbc.sql
+++ datasets/create_mbc.sql
@@ -18,3 +18,37 @@ CREATE TABLE Movies
    CONSTRAINT month FOREIGN KEY (release_date) REFERENCES Months(month)
 );
 
+CREATE VIEW top10Rev AS 
+    SELECT * 
+    FROM Movies 
+    ORDER BY revenue 
+    DESC LIMIT 10;
+
+CREATE VIEW top10Vote AS 
+    SELECT * 
+    FROM Movies 
+    ORDER BY vote_average 
+    DESC LIMIT 10;
+
+CREATE VIEW avgRevPerQ(quarter, average) AS
+    SELECT quarter, AVG(revenue) AS avg_revenue
+    FROM Movies,Months
+    WHERE release_date = month
+    GROUP BY quarter
+    ORDER BY avg_revenue DESC;
+
+CREATE VIEW avgVotePerQ(quarter, vote) AS
+    SELECT quarter, AVG(vote_average) AS vote
+    FROM Movies, Months
+    WHERE release_date = month
+    GROUP BY quarter
+    ORDER BY vote DESC;
+
+CREATE VIEW avgRevPerVote(vote_average, rev) AS
+    SELECT vote_average, AVG(revenue) AS rev
+    FROM Movies, Months
+    WHERE release_date = month
+    GROUP BY vote_average
+    ORDER BY rev DESC;
+
+
